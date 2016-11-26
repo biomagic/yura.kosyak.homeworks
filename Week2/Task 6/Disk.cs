d31eca3c335d55d2be75d24fe5d83aeaa6564721
biomagic <2755527@mail.ru>
2016-11-26 11:33:20 +0200

    Bit of task 6

---
+++ Week2/Task 6/Disk.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+  class Library
+  {
+    public string itemName;
+    public string composer;
+
+    public static void Display(List<Library> lib)
+    {
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      foreach (var item in lib)
+      {
+        Console.WriteLine("{0}, {1}", item.itemName, item.composer);
+      }
+      Console.WriteLine();
+    }
+
+    public static void Filling(List<Library>lib, int n)
+    {
+      for (int i = 1; i <= n; i++)
+      {
+        if (lib.GetType() == typeof(Disk))
+        {
+          lib.Add(new Disk {itemName = "Some disk " + i, composer = "Composer " + i});
+        }
+        lib.Add(new Song { itemName = "Some song " + i, composer = "Composer " + i });
+      }
+    }
+  }
+  class Disk : Library
+  {
+
+  } 
+  class Song : Library
+  {
+    
+  }
+}
---
+++ Week2/Task 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<Library> diskList = new List<Library>();
+      List<Library> songList = new List<Library>();
+
+      // Add disks, songs to collection
+
+      Library.Filling(diskList, 5);
+      Library.Filling(songList, 10);
+
+      // Show disks, songs
+
+      Library.Display(diskList);
+      Library.Display(songList);
+
+      Console.ReadKey();
+    }
+  }
+}
